4ba906acf16beaa6ec0c6e1641b90727d4cd376f
Mladen <58631567+Mladen85@users.noreply.github.com>
2025-01-08 19:08:41 +0100

    Added command list for i3 wm

---
+++ i3wm/i3wm_commands.sh
@@ -0,0 +1,19 @@
+# To install i3 (on Arch):
+#   1. Install xorg-init xorg-server i3-wm i3status dmenu
+#   2. Configure Xinit: echo "exec i3" > ~/.xinitrc
+#   3. Run 'startx' command
+#   4. First time i3 is run, it will ask where to put config file (default '~/.config/i3/config')
+#       and to chose Mod_key (Win key or Alt key). Recommended is Win key not to mix with other shortcuts.
+#   5. Install terminal emulator (Konsole, Urxvt , Alacritty, Kitty...), firefox, and other window applications
+
+
+
+# Quit i3 wm:
+# Mod_key + Shift + E
+
+# Reset i3 wm:
+# Mod_key + Shift + R
+
+# Switch windows Mod_key + Number
+
+# Open dmenu and start typing application name (for example fire) and press Enter to open it
